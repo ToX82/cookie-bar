e141b37419a3c21536720e6cfbd0f1fad82b1117
tox82 <toss82@gmail.com>
2015-03-20 17:26:29 +0100

    First release

---
+++ README.md
@@ -0,0 +1,64 @@
+Permisson Bar
+=============
+
+Cookie Bar is a free & easy solution to the EU cookie law.
+
+##### Why use Cookie Bar?
+
+There is a lot of mystery and fuss surrounding the new EU cookie legislation, but it's essentially really simple. Cookies are files used to track site activity and most websites use them. Site owners need to make the use of cookies very obvious to visitors.
+
+Cookie bar makes it simple and clear to visitors that cookies are in use and tells them how to adjust browser settings if they are concerned.
+
+##### TL;DR
+
+Just place this somewhere in your website and forget it:
+
+    http://cookie-bar.eu/cookiebar-latest.js
+
+##### DEMO
+
+Everyone wants to see a demo before downloading something, isn't it? You can see a demo here:
+
+[http://cookie-bar.eu](http://cookie-bar.eu/)
+
+##### How it works?
+
+Permisson Bar is a drop-in and forget, pure vanilla javascript code, no jQuery or any other dependency needed. It shows up only when actually needed and stay silent if not: If a website has a cookie or some localStorage data set then the bar is shown, otherwhise nothing happens.
+
+Once user clicks 'Allow Cookies' Cookie Bar will set a cookie for that domain with a name 'cookiebar' that will expire in 30 days. What this means is that the plugin will only show up once per domain (per month).
+
+If a user decides to click "Disallow Cookies", Cookies Bar will simply remove all cookies and localStorage data (and will show up again the first time a cookie is detected).
+
+##### How many languages are supported?
+
+Currently, the supported languages for Cookie Bar are:
+
+* it - Italian
+* en - English
+* fr - French
+
+The user language is automatically detected by the browser, but you can force a specific language by passing an optional parameter (see below).
+
+##### How to Install?
+
+You have two options here:
+<ol>
+	<li>Use the hosted version (work in progress, you will see the link here ASAP)</li>
+	<li>Or, 
+		<ol>
+			<li>Grab the Github repo and place it somewhere in a folder within your website. All of the files, including images and stylesheets, need to maintain it's relative structure - that is be in the same directory - for the plugin to work correctly.</li>
+			<li>Put the code you have seen in TL;DR section somewhere on all relevant pages of your website.</li>
+		</ol>
+	</li>
+</ol>
+
+###### If you need to configure it, you can do it like that:
+
+    http://www.cookie-bar.eu/cookiebar-latest.js?forceYes=1&desiredParameters
+
+##### Here is a short list of parameters you can use:
+
+    forceYes=1 <= hides deny consent button and text
+    forceLang=XX <= force a specific language
+    blocking=1 <= blocks all the page until the user clicks deny or consent cookies
+    
---
+++ cookiebar-latest.js
@@ -0,0 +1,303 @@
+/*
+    Plugin Name: Cookie Bar
+    Plugin URL: http://cookiebar.com/
+    @author: Emanuele "ToX" Toscano
+    @description: Cookie Bar is a free & simple solution to the EU cookie law.
+    @version: 2.0
+*/
+
+/*
+ * Available languages array
+ */
+var CookieLanguages = [
+    'en',
+    'it',
+    'fr'
+];
+
+/**
+ * Main function
+ */
+function setupCookieBar() {
+
+    /**
+     * Load plugin only if needed (do nothing if cookiebar cookie is set)
+     * @param null
+     * @return null
+     */
+    if (document.cookie.length > 0 || window.localStorage.length > 0) {
+        var accepted = getCookie("cookiebar");
+        if (accepted === undefined) {
+            scriptPath = getScriptPath();
+
+            loadAssets();
+            prepareActions();
+            fadeIn(cookieBar, 250);
+            setBodyMargin();
+        }
+    }
+
+    /**
+     * Get this javascript's path
+     * @param null
+     * @return {String} this javascript's path
+     */
+    function getScriptPath() {
+        var scripts = document.getElementsByTagName("script");
+
+        for (var i = 0; i < scripts.length; i++) {
+            if (scripts[i].hasAttribute("src")) {
+                var path = scripts[i].src;
+                if (path.indexOf("cookiebar") >-1) {
+                    return path;
+                }
+            }
+        }
+    }
+
+    /**
+     * Load external files (css, language files etc.)
+     * @param null
+     * @return null
+     */
+    function loadAssets() {
+        var userLang = detectLang();
+
+        // Load CSS file
+        path = scriptPath.replace(/[^\/]*$/, "");
+        var stylesheet = document.createElement("link");
+        stylesheet.setAttribute("rel", "stylesheet");
+        stylesheet.setAttribute("href", path + "cookiebar.css");
+        document.head.appendChild(stylesheet);
+
+        // Load the correct language messages file and set some variables
+        var request = new XMLHttpRequest();
+        request.open('GET', path + "/lang/" + userLang + ".html", false);
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                var element = document.createElement('div');
+                var resp = request.responseText;
+                element.innerHTML = resp;
+                document.getElementsByTagName('body')[0].appendChild(element);
+
+                cookieBar = document.getElementById('cookie-bar');
+                button = document.getElementById('cookie-bar-button');
+                buttonNo = document.getElementById('cookie-bar-button-no');
+                prompt = document.getElementById('cookie-bar-prompt');
+
+                promptBtn = document.getElementById('cookie-bar-prompt-button');
+                promptClose = document.getElementById('cookie-bar-prompt-close');
+                promptContent = document.getElementById('cookie-bar-prompt-content');   
+                promptNoConsent = document.getElementById('cookie-bar-no-consent');
+                cookiesListDiv = document.getElementById('cookies-list');
+
+                if (!getURLParameter("showNoConsent")) {
+                    promptNoConsent.style.display = "none";
+                    buttonNo.style.display = "none";
+                }
+
+                if (getURLParameter("blocking")) {
+                    fadeIn(prompt, 500);
+                    promptClose.style.display = "none";
+                }
+
+                if (getURLParameter("top")) {
+                    cookieBar.style.top = 0;
+                    setBodyMargin("top");
+                } else {
+                    cookieBar.style.bottom = 0;
+                    setBodyMargin("bottom");
+                }
+
+                listCookies(cookiesListDiv);
+            }
+        };
+        request.send();
+    }
+
+
+    /**
+     * Get browser's language or, if available, the specified one
+     * @param null
+     * @return {String} userLang - short language name
+     */
+    function detectLang() {
+        var userLang = getURLParameter("forceLang");
+        if (userLang === false) {
+            userLang = navigator.language || navigator.userLanguage;
+        }
+        userLang = userLang.substr(0,2);
+        if (CookieLanguages.indexOf(userLang) < 0) {
+            userLang = "en";
+        }
+        return userLang;
+    }
+
+    /**
+     * Get Cookie Bar's cookie if available
+     * @param {string} c_name - cookie name
+     * @return {string} cookie value
+     */
+    function getCookie(c_name) {
+        var i, x, y, ARRcookies = document.cookie.split(";");
+        for (i = 0; i < ARRcookies.length; i++) {
+            x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+            y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+            x = x.replace(/^\s+|\s+$/g, "");
+            if (x == c_name) {
+                return unescape(y);
+            }
+        }
+    }
+
+    /**
+     * Get a list of all cookies
+     * @param NULL
+     * @return {array} cookies list
+     */
+    function listCookies(cookiesListDiv) {
+        var cookies = [];
+        var i, x, y, ARRcookies = document.cookie.split(";");
+        for (i = 0; i < ARRcookies.length; i++) {
+            x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+            y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+            x = x.replace(/^\s+|\s+$/g, "");
+            cookies.push(x);
+        }
+        cookiesListDiv.innerHTML = cookies.join(", ");
+    }
+
+    /**
+     * Get Cookie Bar's cookie if available
+     * @param {string} c_name - cookie name
+     * @param {string} value - cookie value
+     * @param {string} exdays - expiration days
+     * @return null
+     */
+    function setCookie(c_name, value, exdays) {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + exdays);
+        var c_value = escape(value) + ((exdays === null) ? "" : "; expires=" + exdate.toUTCString());
+        document.cookie = c_name + "=" + c_value;
+    }
+
+    /**
+     * Remove all the cookies and empty localStorage
+     * @param null
+     * @return null
+     */
+    function removeCookie() {
+        // Clear cookies
+        document.cookie.split(";")
+            .forEach(function (c) {
+                document.cookie = c.replace(/^ +/, "")
+                    .replace(/=.*/, "=;expires=" + new Date()
+                        .toUTCString() + ";path=/");
+            });
+        // Clear localStorage
+        localStorage.clear();
+    }
+
+
+    /**
+     * FadeIn effect
+     * @param {string} el - element name
+     * @param {string} speed - effect duration
+     * @return null
+     */
+    function fadeIn(el, speed) {
+        var s = el.style;
+        s.opacity = 0;
+        s.display = "block";
+        (function fade() {
+            (s.opacity -= -0.1) > 0.9 ? null : setTimeout(fade, (speed/10));
+        })();
+    }
+
+
+    /**
+     * FadeOut effect
+     * @param {string} el - element name
+     * @param {string} speed - effect duration
+     * @return null
+     */
+    function fadeOut(el, speed) {
+        var s = el.style;
+        s.opacity = 1;
+        (function fade() {
+            (s.opacity -= 0.1) < 0.1 ? s.display = "none" : setTimeout(fade, (speed/10));
+        })();
+    }
+
+    /**
+     * Add a body tailored bottom margin so that CookieBar doesn't hide anything
+     * @param null
+     * @return null
+     */
+    function setBodyMargin(where) {    
+        setTimeout(function () {
+            var height;
+            switch (where) {
+                case "top": 
+                    height = document.getElementById("cookie-bar").clientHeight;
+                    document.getElementsByTagName('body')[0].style.marginTop = height + "px";
+                    break;
+                case "bottom":
+                    height = document.getElementById("cookie-bar").clientHeight;
+                    document.getElementsByTagName('body')[0].style.marginBottom = height + "px";
+                    break;
+            }
+        }, 260);
+    }
+
+    /**
+     * GET parameter to look for
+     * @param {string} name - param name
+     * @return {string} param value (false if parameter is not found)
+     */
+    function getURLParameter(name) {
+        var set = unescape(scriptPath).split(name + "=");
+        if (set[1]) {
+            return set[1].split(/[&?]+/)[0];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Button actions
+     * @param null
+     * @return null
+     */
+    function prepareActions() {
+        button.addEventListener('click', function () {
+            setCookie("cookiebar", "CookieAllowed", 30);
+            fadeOut(prompt, 250);
+            fadeOut(cookieBar, 250);
+        });
+
+        buttonNo.addEventListener('click', function () {
+            var txt = promptNoConsent.innerText;
+            var confirm = window.confirm(txt);
+            if (confirm === true) {
+                removeCookie();
+                fadeOut(prompt, 250);
+                fadeOut(cookieBar, 250);
+            }
+        });
+
+        promptBtn.addEventListener('click', function () {
+            fadeIn(prompt, 250);
+        });
+
+        promptClose.addEventListener('click', function () {
+            fadeOut(prompt, 250);
+        });
+    }
+}
+
+
+// Load the script only if there is at least a cookie or a localStorage item
+document.addEventListener("DOMContentLoaded", function () {
+    setupCookieBar();
+});
